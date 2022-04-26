08221c3bcc865148f0a197890efd4f506e348148
João Gabriel Bento de Paiva <102361524+jgbdp203@users.noreply.github.com>
2022-04-26 12:09:34 -0300

    Add files via upload

--- nota.c
+++ nota.c
@@ -9,7 +9,7 @@ soma=0;
 na=0;
 nb=0;
 printf("Digite uma nota\n");
-scanf("%d", nota);
+scanf("%d", &nota);
 while(nota>=0) {
 printf("Digite uma nota\n");
     if(nota>=0)
@@ -22,14 +22,14 @@ printf("Digite uma nota\n");
 
     else if(nota>=5)
         na++;
-scanf("%d", nota);
+scanf("%d", &nota);
 }
 media=(soma)/(na+nb);
 porcentagem=(nb/(na+nb))*100;
-printf("O numero de alunos coom nota baixa eh: %d", nb);
-printf("O numero de alunos coom nota alta eh: %d", na);
-printf("A media das notas eh: %d", media);
-printf("A porcentagem de alunos com nota maior que a média eh: %d", porcentagem);
+printf("O numero de alunos coom nota baixa eh: %d\n", nb);
+printf("O numero de alunos coom nota alta eh: %d\n", na);
+printf("A media das notas eh: %d\n", media);
+printf("A porcentagem de alunos com nota maior que a média eh: %d\n", porcentagem);
 return 0;}
 
 
